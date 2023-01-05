e457bfd8021db175ac32486ebca8de1928c37097
Prayush Kumar <prayush.kumar@gmail.com>
2023-01-05 19:15:34 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,12 +1,4 @@
-# gwnrtools
+# Template Repository
 
-A collection of tools for academic research in gravitational-wave astronomy & astrophysics, and numerical relativity
+Library for ...
 
-## Tutorials
-
- * [Making useful corner plots to visualize Bayesian posteriors](tutorials/MakingUsefulCornerPlots.html)
- * [Faithfulness between GW template models](tutorials/ComputeFaithfulness.html)
- * [Effectualness of GW search template banks](tutorials/ComputeEffectualness.html)
- * [Inference on GW events](tutorials/BayesianInferenceOnGWEvents.html)
- * [Inference on Injections](tutorials/BayesianInferenceOnGWInjections.html)
- * ... more [here](https://github.com/prayush/gwnrtools/tree/master/tutorials)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Copyright (c) 2018, Prayush Kumar
-# See LICENSE file for details: <https://github.com/gwnrtools/gwnrtools/blob/master/LICENSE>
+# See LICENSE file for details: <https://github.com/gwnrtools/template/blob/master/LICENSE>
 
 from __future__ import print_function
 
@@ -9,8 +9,8 @@ from os import environ, path
 import subprocess
 from pathlib import Path
 
-NAME = 'gwnr'
-VERSION = 'v2021.09.20'
+NAME = 'package-name-here'
+VERSION = 'v0.0.0'
 
 
 def write_version_file(version):
@@ -94,59 +94,16 @@ if __name__ == "__main__":
         'A collection of tools for academic research in gravitational-wave astronomy & astrophysics',
         long_description=get_long_description(),
         license="GPL",
-        url='https://github.com/gwnrtools/gwnrtools',
-        author='Prayush Kumar',
-        author_email='prayush.kumar@gmail.com',
+        url='https://github.com/gwnrtools/template',
+        author='Who Am I?',
+        author_email='What Is My Email Address?',
         packages=find_packages(),
         package_dir={NAME: NAME},
         package_data={
             # version info
             NAME: [write_version_file(VERSION)],
-            'gwnr.data': ['gw_noise_curves/*.txt', 'gw_noise_curves/*.dat']
+            'template.data': []
         },
-        install_requires=[
-            'astropy>=2.0.16',
-            'bilby',
-            'h5py>=2.10.0',
-            # 'lalsuite>=6.63',
-            'lscsoft_glue>=2.0.0',
-            'matplotlib>=2.1',
-            'numexpr',
-            'numpy>=1.16.5',
-            'pandas>=0.24.2',
-            'pathlib',
-            # 'pycbc>=1.15.4',
-            'pyswarm>=0.6',
-            'pytest',
-            'romspline>=1.1.6',
-            'scikit-learn>=0.20.4',
-            'scipy>=1.2.3',
-            'seaborn>=0.9.1',
-            'six>=1.10.0',
-            'statsmodels>=0.10.2',
-            'utils>=0.9.0'
-        ],
-        scripts=[
-            'bin/utils/makepdf', 'bin/banksim_generic.py',
-            'bin/choose_testpoints.py', 'bin/choose_best_testpoints.py',
-            'bin/remove_eliminated_testpoints.py',
-            'bin/split_table_geometrically.py',
-            'bin/gwnr_create_bank_workflow',
-            'bin/gwnr_create_banksim_workflow',
-            'bin/gwnr_create_faithsim_workflow',
-            'bin/gwnr_write_pycbc_inference_configs',
-            'bin/gwnr_create_injections_pycbc_inference_workflow',
-            'bin/gwnr_create_public_events_pycbc_inference_workflow',
-            'bin/gwnr_write_bilby_configs',
-            'bin/gwnr_create_injections_bilby_workflow',
-            'bin/gwnr_create_public_events_bilby_workflow', 'bin/gwnr_banksim',
-            'bin/gwnr_faithsim', 'bin/gwnr_force_success_from_condor_sub',
-            'bin/gwnr_sample_parameter_space',
-            'bin/gwnr_enigma_plan_calib_grid_and_make_dag',
-            'bin/gwnr_enigma_sample_calib_parameters',
-            'bin/utils/toggle_lsctable_type', 'bin/utils/ConvertHTMLToIpynb',
-            'bin/analysis/ComputeOptimalSNRForGWSignals.py',
-            'bin/nr/JoinDatainHDF',
-            'bin/stats/ComputeInferredParametersFromLIPosterior.py'
-        ],
+        install_requires=[],
+        scripts=[],
     )
