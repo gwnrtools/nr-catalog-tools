852759b791e90fd538bf61bc74d9387d0e82845b
Prayush Kumar <prayush.kumar@gmail.com>
2023-04-07 02:43:58 +0530

    Move RIT catalog specifics/configs into utils
    Also added a few general utility methods in utils

--- nrcatalogtools/utils.py
+++ nrcatalogtools/utils.py
@@ -14,6 +14,106 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import lal
 import numpy as np
+import pathlib
+import requests
+import sxs
+
+## --------------------------------------------------------------
+nrcatalog_cache_dir = pathlib.Path('~/.nr_data/').expanduser().resolve()
+
+rit_catalog_info = {}
+rit_catalog_info['cache_dir'] = nrcatalog_cache_dir / 'RIT'
+rit_catalog_info['metadata_dir'] = rit_catalog_info['cache_dir'] / 'metadata'
+rit_catalog_info['data_dir'] = rit_catalog_info['cache_dir'] / 'data'
+
+rit_catalog_info['url'] = 'https://ccrgpages.rit.edu/~RITCatalog/'
+rit_catalog_info['metadata_url'] = rit_catalog_info['url'] + '/Metadata/'
+rit_catalog_info['data_url'] = rit_catalog_info['url'] + '/Data/'
+rit_catalog_info['possible_resolutions'] = [
+    100, 120, 88, 118, 130, 140, 144, 160, 200
+]
+
+rit_catalog_info['metadata_file_fmts'] = [
+    'RIT:BBH:{:04d}-n{:3d}-id{:d}_Metadata.txt',
+    'RIT:eBBH:{:04d}-n{:3d}-ecc_Metadata.txt',
+]
+rit_catalog_info['waveform_file_fmts'] = [
+    'ExtrapStrain_RIT-BBH-{:04d}-n{:3d}.h5',
+    'ExtrapStrain_RIT-eBBH-{:04d}-n{:3d}.h5',
+]
+rit_catalog_info['max_id_val'] = 6
+
+maya_catalog_info = {
+    'cache_dir': nrcatalog_cache_dir / 'MAYA',
+    'url': '',
+    'metadata_file_fmts': [],
+    'waveform_file_fmts': [],
+}
+
+## --------------------------------------------------------------
+
+
+def url_exists(link, num_retries=100):
+    """Check if a given URL exists on the web.
+
+    Args:
+        link : complete web URL
+
+    Returns:
+        bool: True/False whether the URL could be found on WWW.
+    """
+    requests.packages.urllib3.disable_warnings()
+    for n in range(num_retries):
+        try:
+            response = requests.head(link, verify=False)
+            if response.status_code == requests.codes.ok:
+                return True
+            else:
+                return False
+        except:
+            continue
+    return False
+
+
+def download_file(url, path, progress=False, if_newer=True):
+    if url_exists(url):
+        return sxs.utilities.downloads.download_file(url,
+                                                     path,
+                                                     progress=progress,
+                                                     if_newer=if_newer)
+    return None
+
+
+def call_with_timeout(myfunc, args=(), kwargs={}, timeout=5):
+    '''
+    This function calls user-provided `myfunc` with user-provided
+    `args` and `kwargs` in a separate multiprocessing.Process.
+    if the function evaluation takes more than `timeout` seconds, the
+    `Process` is terminated and error raised. If it evalutes within
+    `timeout` seconds, the results are fetched from the `Queue` and
+    returned.
+    '''
+    from multiprocessing import (Process, Queue)
+
+    def funcwrapper(p, *args, **kwargs):
+        '''
+        This thin wrapper calls the user-provided function, and puts
+        its result into the multiprocessing `Queue`  so that it can be
+        obtained via `Queue().get()`.
+        '''
+        res = myfunc(*args, **kwargs)
+        p.put(res)
+
+    queue = Queue()
+    task = Process(target=funcwrapper, args=(queue, *args))
+    task.start()
+    task.join(timeout=timeout)
+    task.terminate()
+    try:
+        result = queue.get(timeout=0)
+        return result
+    except:
+        raise Exception("Timeout")
 
 
 def time_to_physical(M):
