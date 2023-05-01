afd9d6b248cab9338448e686c2b4059693d0250a
Prayush Kumar <prayush.kumar@gmail.com>
2023-05-02 00:27:47 +0530

    Use get_metadata instead of reconstructing paths

--- nrcatalogtools/rit.py
+++ nrcatalogtools/rit.py
@@ -125,8 +125,7 @@ class RITCatalog(catalog.CatalogBase):
         return self._helper.waveform_filename_from_simname(sim_name)
 
     def waveform_filepath_from_simname(self, sim_name):
-        file_path = self._helper.waveform_data_dir / self.waveform_filename_from_simname(
-            sim_name)
+        file_path = self.get_metadata(sim_name)['waveform_data_location']
         if not os.path.exists(file_path):
             raise RuntimeError(f"Could not resolve path for {sim_name}"
                                f"..best calculated path = {file_path}")
@@ -140,12 +139,11 @@ class RITCatalog(catalog.CatalogBase):
         return self._helper.metadata_filename_from_simname(sim_name)
 
     def metadata_filepath_from_simname(self, sim_name):
-        file_path = self._helper.metadata_dir / self.metadata_filename_from_simname(
-            sim_name)
+        file_path = self.get_metadata(sim_name)['metadata_location']
         if not os.path.exists(file_path):
             raise RuntimeError(f"Could not resolve path for {sim_name}"
                                f"..best calculated path = {file_path}")
-        return file_path.as_posix()
+        return str(file_path)
 
     def metadata_url_from_simname(self, sim_name):
         return self._helper.metadata_url + "/" + self.metadata_filename_from_simname(
