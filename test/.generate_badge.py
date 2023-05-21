b0f2147443a59aea28ae4f35e59b0a8cb3deb2f7
vaishakp <vaishakprasad@gmail.com>
2023-05-21 08:15:33 +0530

    add badge generator for coverage

--- test/.generate_badge.py
+++ test/.generate_badge.py
@@ -1,16 +1,18 @@
-from PIL import Image, ImageDraw, ImageFont
 from coverage import coverage
 
 cov = coverage()
 cov.load()
-total_cov = cov.report()
+total_cov = round(cov.report())
 
-badge = Image.new("RGB", (140, 60))
-font = ImageFont.load_default()
 
-bd = ImageDraw.Draw(im)
 
-bd.text((10, 5), "coverage:", fill=(255, 255, 255), font=font)
-bd.rectangle([(80, 0), (150, 20)], fill=(220, 0, 0))
-bd.text((90, 5), "{:.0f}%".format(total_cov), fill=(0, 0, 0), font=font)
-badge.save('cov_badge.jpg')
+badge = f"""<svg width="120" height="20" xmlns="http://www.w3.org/2000/svg">
+  <rect width="80" height="20" rx="0" ry="5" fill="black" x="0" y="0" />
+  <rect width="40" height="20" fill="red" x="80" y="0" />
+  <text x="6" y="13" fill="white" font-size="14">Coverage  </text>
+  <text x="85" y="14" fill="black" font-size="14">{total_cov}% </text>
+</svg>
+"""
+
+with open("svg_badge.svg", "w") as svgf:
+    svgf.write(badge)
