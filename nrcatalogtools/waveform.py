999db0d096f45d72ac0bd41992e1920efd76c726
Prayush Kumar <prayush.kumar@gmail.com>
2024-12-15 19:46:30 +0530

    Read tar.gz
    Add a method to WaveformModes to read in data from tar.gz archives

--- nrcatalogtools/waveform.py
+++ nrcatalogtools/waveform.py
@@ -26,7 +26,6 @@ from sxs.waveforms.format_handlers.nrar import (
 
 ELL_MIN, ELL_MAX = 2, 10
 
-
 class WaveformModes(sxs_WaveformModes):
     def __new__(
         cls,
@@ -215,7 +214,6 @@ class WaveformModes(sxs_WaveformModes):
             raise RuntimeError(f"Could not use or open {file_path}")
 
         import quaternionic
-        import re
         import tarfile
 
         def get_tag(name):
@@ -232,7 +230,9 @@ class WaveformModes(sxs_WaveformModes):
         cls._filepath = file_path
 
         # If _metadata is not already a set attribute, then set it here.
-        if not hasattr(cls, "_metadata"):
+        try:
+            cls._metadata
+        except AttributeError:
             cls._metadata = metadata
 
         ell_min, ell_max = 99, -1
@@ -240,66 +240,31 @@ class WaveformModes(sxs_WaveformModes):
 
         file_tag = get_tag(file_path)
         mode_data = {}
-        reference_mode_num_for_length = ()
-        possible_ascii_extensions = ["asc", "dat", "txt"]
-
         with tarfile.open(file_path, "r:gz") as tar:
-            if verbosity > 4:
-                print(f"Opening tarfile: {file_path}")
             for dat_file in tar.getmembers():
                 dat_file_name = dat_file.name
-                if verbosity > 4:
-                    print(f"dat_file_name is: {dat_file_name}")
-                if file_tag not in dat_file_name or np.all(
-                    [
-                        f".{ext}" not in dat_file_name
-                        for ext in possible_ascii_extensions
-                    ]
-                ):
-                    if verbosity > 5:
-                        print(
-                            f"{file_tag} not in {dat_file_name} is {file_tag not in dat_file_name}"
-                        )
-                        print(
-                            "the other flag is: ",
-                            np.all(
-                                [
-                                    f".{ext}" not in dat_file_name
-                                    for ext in possible_ascii_extensions
-                                ]
-                            ),
-                        )
+                if file_tag not in dat_file_name or ".asc" not in dat_file_name:
                     continue
                 ell, em = get_el_em_from_filename(dat_file_name)
                 with tar.extractfile(dat_file_name) as f:
-                    reference_mode_num_for_length = (ell, em)
                     mode_data[(ell, em)] = np.loadtxt(f)
-                    # Convert to row-major form
-                    nrows, ncols = np.shape(mode_data[(ell, em)])
-                    if nrows < ncols:
-                        mode_data[(ell, em)] = mode_data[(ell, em)].T
                     # mode_data[get_tag(dat_file_name)] = np.loadtxt(f)
                 # get the minimum time and maximum time stamps for all modes
                 t_min = max(t_min, mode_data[(ell, em)][0, 0])
                 t_max = min(t_max, mode_data[(ell, em)][-1, 0])
                 dt = min(
                     dt,
-                    stat_mode(np.diff(mode_data[(ell, em)][:, 0]), keepdims=True)[0][0],
+                    stat_mode(np.diff(mode_data[(ell, em)][:, 0]), keepdims=True)[0][
+                        0
+                    ]
                 )
                 ell_min = min(ell_min, ell)
                 ell_max = max(ell_max, ell)
 
-        # We populate LM here because it has to be ordered, as the WaveformModes
-        # class expects an ordered data set.
         LM = []
         for ell in range(ELL_MIN, ELL_MAX + 1):
             for em in range(-ell, ell + 1):
                 if (ell, em) in mode_data:
-                    LM.append([ell, em])
-                else:
-                    reference_mode = mode_data[reference_mode_num_for_length]
-                    mode_data[(ell, em)] = np.zeros(np.shape(reference_mode))
-                    mode_data[(ell, em)][:, 0] = reference_mode[:, 0]  # Time axis
                     LM.append([ell, em])
 
         if len(LM) == 0:
@@ -312,16 +277,10 @@ class WaveformModes(sxs_WaveformModes):
         times = np.arange(t_min, t_max + 0.5 * dt, dt)
         data = np.empty((len(times), len(LM)), dtype=complex)
         for idx, (ell, em) in enumerate(LM):
-            mode_time, mode_real, mode_imag = (
-                mode_data[(ell, em)][:, 0],
-                mode_data[(ell, em)][:, 1],
-                mode_data[(ell, em)][:, 2],
-            )
-            if verbosity > 5:
-                print(f"Interpolating mode {ell}, {em}. Data length: {len(mode_time)}")
+            mode_time, mode_real, mode_imag = mode_data[(ell, em)]
             mode_real_interp = InterpolatedUnivariateSpline(mode_time, mode_real)
             mode_imag_interp = InterpolatedUnivariateSpline(mode_time, mode_imag)
-            data[:, idx] = mode_real_interp(times) + 1j * mode_imag_interp(times)
+            data[:, idx] = mode_real_interp(times) * 1j * mode_imag_interp(times)
 
         w_attributes = {}
         w_attributes["metadata"] = metadata
