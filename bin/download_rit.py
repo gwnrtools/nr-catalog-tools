e22d89527be06f48a55a24f180aabc994bc4200b
Prayush Kumar <prayush.kumar@gmail.com>
2025-07-22 21:27:57 +0530

    Use waveform data for bulk download of RIT catalog by default

--- bin/download_rit.py
+++ bin/download_rit.py
@@ -4,4 +4,4 @@ from nrcatalogtools.rit import RITCatalog
 
 rc = RITCatalog.load(verbosity=5, download=True)
 
-rc.download_data_for_catalog(which_data="psi4")
+rc.download_data_for_catalog(which_data="waveform") # psi4
